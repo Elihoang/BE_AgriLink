e2864ccc20bf8ce31775b2f8a5c114ca10ab1481
Lê Duy Hoàng <129831728+Elihoang@users.noreply.github.com>
2026-01-13 11:42:55 +0700

    Downgrade NuGet package versions for .NET 9.0
    This commit updates several NuGet package references to lower versions in AgriLink_DH.Api, including Microsoft.AspNetCore.OpenApi, EntityFrameworkCore.Design, Scalar.AspNetCore, Serilog, Swashbuckle.AspNetCore, and System.IdentityModel.Tokens.Jwt. The change affects project assets, build props, and targets to ensure compatibility with the desired package versions.

--- AgriLink_DH.Api/obj/Debug/net9.0/AgriLink_DH.Api.AssemblyInfo.cs
+++ AgriLink_DH.Api/obj/Debug/net9.0/AgriLink_DH.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgriLink_DH.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819b4c845463c28401c88d997b03cc993c118fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c812b96e866e1329ba067c3cd3a0658baf4ba35a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgriLink_DH.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgriLink_DH.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
